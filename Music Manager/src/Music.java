e0efc46159baada1a92d97133045a46170906b5d
caion <caion@DESKTOP-GOITPTT>
2020-09-30 17:43:18 -0300

    Criação de classes e atributos base

--- Music Manager/src/Music.java
+++ Music Manager/src/Music.java
@@ -1,8 +1,87 @@
+import java.util.ArrayList;
 
 public class Music {
-	public static void main(String[] args) {
-		System.out.println("teste");
-		System.out.println("\nTesde de commit 1");
+	//Classe que vai informar os dados das músicas para a criação das playlists
+	private String musicName;
+	private ArrayList<String> musicGenre;
+	private int musicID;
+	private String musicArtist;
+	private String musicRecorder;
+	
+	//Construtores e get/sets
+	public Music(String musicName,ArrayList<String> musicGenre,int musicID,
+			String musicArtist,String musicRecorder) {}
+	
+	
+	
+	public String getMusicName() {
+		return musicName;
+	}
+
+
+
+	public void setMusicName(String musicName) {
+		this.musicName = musicName;
+	}
+
+
+
+	public ArrayList<String> getMusicGenre() {
+		return musicGenre;
+	}
+
+
+
+	public void setMusicGenre(ArrayList<String> musicGenre) {
+		this.musicGenre = musicGenre;
+	}
+
+
+
+	public int getMusicID() {
+		return musicID;
+	}
+
+
+
+	public void setMusicID(int musicID) {
+		this.musicID = musicID;
+	}
+
+
+
+	public String getMusicArtist() {
+		return musicArtist;
+	}
+
+
+
+	public void setMusicArtist(String musicArtist) {
+		this.musicArtist = musicArtist;
+	}
+
+
+
+	public String getMusicRecorder() {
+		return musicRecorder;
+	}
+
+
+
+	public void setMusicRecorder(String musicRecorder) {
+		this.musicRecorder = musicRecorder;
+	}
+
+
+//Métodos
+	public boolean equals(Music outraMusic) 
+	{
+		return true;
+	}
+	
+	public String toString() 
+	{
+		return "retornar algo aqui";
 	}
 
 }
