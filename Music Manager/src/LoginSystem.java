47fcc4e061f06b8f33b3a53ab2f23a9652539b7d
Murilo Mota <murilohfmota@gmail.com>
2020-10-03 19:45:49 -0300

    Criado classe loginSystem

--- Music Manager/src/LoginSystem.java
+++ Music Manager/src/LoginSystem.java
@@ -5,11 +5,11 @@ public class LoginSystem {
     private String emailInput;
     private String passWordInput;  
 
-    public void inputEmailAndPassWord(){
+    public void inputEmailAndPassWord(){// trata os dados do input para teste de validação.
         Scanner email = new Scanner(System.in);
     }
 
-    public boolean validateInput(){
+    public boolean validateInput(){// verifica se os dados do input são validos para acesso.
         return true;
     }
 
