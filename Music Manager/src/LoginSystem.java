8f45164be4ee0b4eb414600f000308d901c700d0
Murilo Mota <murilohfmota@gmail.com>
2020-10-03 19:41:21 -0300

    arrumando o git

---
+++ Music Manager/src/LoginSystem.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class LoginSystem {
+    
+    private String emailInput;
+    private String passWordInput;  
+
+    public void inputEmailAndPassWord(){
+        Scanner email = new Scanner(System.in);
+    }
+
+    public boolean validateInput(){
+        return true;
+    }
+
+}
---
+++ Music Manager/src/search.java
@@ -0,0 +1,22 @@
+public class search {
+    
+    private String nameInput; // input do usuario de nome;
+    private genres genreInput; // input usuario para genero
+    private String artistInput; //input usuario para artista
+    private String recorderInput; //input usuario para gravadora
+    private String durationInput; //input usuario para duaração da musica
+
+
+    //-----metodos-----//
+    public void userInput(){// precurar playlist de usuarios.
+
+    }
+
+    public void filterPlaylists(){// Filtrar playlist, varre arrayList de playlists.
+
+    }
+    
+    public void filterMusic(){// filtra musicas. varre arrayList de musicas.
+        
+    }
+}
