8b88c3b28564d555c9d426b1d64e524a08d8dc94
Murilo Mota <murilohfmota@gmail.com>
2020-10-08 16:24:12 -0300

    criado LoginController

---
+++ Music Manager/src/LoginController.java
@@ -0,0 +1,17 @@
+public class LoginController {
+    
+    
+    
+    public boolean validadeInput(String emailInput, String passwordInput){
+        return true;
+    }
+    public void editName(){
+
+    }
+    public void editpassword(){
+
+    }
+    public void editEmail(){
+        
+    }
+}
