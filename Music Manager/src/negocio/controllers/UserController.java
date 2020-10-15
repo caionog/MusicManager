0b342ddd5b510a640d7d02cf3e053404b6f6bae9
Murilo Mota <murilohfmota@gmail.com>
2020-10-15 13:57:28 -0300

    adicionado metodos abstratos em IUserController e implementados em UserController

--- Music Manager/src/negocio/controllers/UserController.java
+++ Music Manager/src/negocio/controllers/UserController.java
@@ -1,5 +1,29 @@
 package negocio.controllers;
 
-public class UserController {
+import data.UserRepo;
+import negocio.User;
+import negocio.interfaces.IUserController;
+
+public class UserController implements IUserController{
+    private UserRepo repositorioUsuario = new UserRepo();
+    
+    
+    @Override
+    public boolean mudarNome(int usuarioAtual) {
+        User usuario = this.repositorioUsuario.getUser(usuarioAtual);
+        usuario.
+    }
+
+    @Override
+    public boolean mudarSenha() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public boolean mudarEmail() {
+        // TODO Auto-generated method stub
+        return false;
+    }
   
 }
--- Music Manager/src/negocio/interfaces/IUserController.java
+++ Music Manager/src/negocio/interfaces/IUserController.java
@@ -1,5 +1,8 @@
 package negocio.interfaces;
+import data.UserRepo;
+public interface IUserController {
+  boolean mudarNome(int usuarioAtual);
+  boolean mudarSenha();
+  boolean mudarEmail();
 
-public class IUserController {
-  
 }
